e83203fabd404ee557d86955229d39b4221342e0
JovanGREINER <jovandelemer30@gmail.com>
2026-03-10 15:11:39 +0100

    Suppression du schema de base

--- src/controller.cpp
+++ src/controller.cpp
@@ -5,15 +5,3 @@
 #include <iostream>
 #endif
 
-void jouer()
-{
-    std::string message = "Le jeu du PENDU va bientôt commencer !";
-
-// exemple de débogage pour une variable
-#ifdef DEBUG_CONTROLLER
-    std::cout << "[" << __FILE__ << ":" << __PRETTY_FUNCTION__ << ":" << __LINE__ << "] "
-              << "message = " << message << std::endl;
-#endif
-
-    afficherMessage(message);
-}
--- src/pendu.cpp
+++ src/pendu.cpp
@@ -2,7 +2,6 @@
 
 int main()
 {
-    jouer();
 
     return 0;
 }
