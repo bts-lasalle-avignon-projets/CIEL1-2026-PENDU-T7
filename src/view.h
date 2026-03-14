ba968b7a419dcae61b4e051f55e5fe8b27ad11f1
aborgellamarque <antoninborgellamarque@gmail.com>
2026-03-14 18:20:58 +0100

    Ajout fonction pour afficher le nombre d'erreurs restantes

--- src/view.h
+++ src/view.h
@@ -6,6 +6,7 @@
 void afficherTitre();
 void demanderNomJoueur(char *nom);
 void afficherMotATrouver(char *motATrouver);
+void afficherErreurs(int erreurs; int erreursMax);
 char demanderLettre();
 
 #endif // VIEW_H
