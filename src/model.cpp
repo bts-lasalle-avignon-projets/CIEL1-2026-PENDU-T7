8a07fd94e4d7f299bff86eef26d6af026044fd10
A.borgellamarque <antoninborgellamarque@gmail.com>
2026-03-16 15:05:31 +0100

    Correction des types de variable

--- src/model.cpp
+++ src/model.cpp
@@ -9,25 +9,25 @@ using namespace std;
 char mots[][MAX_LETTRES] = { "ordinateur", "clavier",       "linux", "processus", "python",
                              "windows",    "programmation", "java",  "souris" };
 
-void initialiserPartie(Partie* partie)
+void initialiserPartie(Partie *partie)
 {
-    partie->erreurs    = 0;
+    partie->erreurs = 0;
     partie->erreursMax = NB_MAX_ERREURS;
 
     choisirMotSecret(partie);
     initialiserMotATrouver(partie);
 }
 
-void choisirMotSecret(Partie* partie)
+void choisirMotSecret(Partie *partie)
 {
     int indiceMotSecret;
-    int nbMots = sizeof(mots) / sizeof(mots[0]); // Nombre de mots dans le tableau.
+    int nbMots = sizeof(mots) / sizeof(mots[0]); //Nombre de mots dans le tableau.
     srand(time(NULL));
     indiceMotSecret = rand() % nbMots;
     strcpy(partie->motSecret, mots[indiceMotSecret]);
 }
 
-void initialiserMotATrouver(Partie* partie)
+void initialiserMotATrouver(Partie *partie)
 {
     int longueurMot = strlen(partie->motSecret);
     for(int i = 0; i < longueurMot; i++)
@@ -37,7 +37,7 @@ void initialiserMotATrouver(Partie* partie)
     partie->motATrouver[longueurMot] = '\0';
 }
 
-void mettreAJourMotATrouver(Partie* partie, char lettre)
+void mettreAJourMotATrouver(Partie *partie, char lettre)
 {
     int longueurMot = strlen(partie->motSecret);
     for(int i = 0; i < longueurMot; i++)
@@ -49,17 +49,17 @@ void mettreAJourMotATrouver(Partie* partie, char lettre)
     }
 }
 
-bool testerVictoire(Partie* partie)
+bool testerVictoire(Partie *partie)
 {
     return partie->motSecret == partie->motATrouver;
 }
 
-bool testerDefaite(Partie* partie)
+bool testerDefaite(Partie *partie)
 {
     return partie->erreurs >= partie->erreursMax;
 }
 
-int verifierLettre(Partie* partie, char lettre)
+int verifierLettre(Partie *partie, char lettre)
 {
     if(!isalpha(lettre))
     {
